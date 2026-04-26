22ca1d08ff235ab78338c40828929735b19a8d94
Jason Scott <jason.scott.sf@gmail.com>
2026-04-26 10:01:06 -0700

    fix: add missing useEffect import to SecurityTab

--- frontend/src/components/SecurityTab.jsx
+++ frontend/src/components/SecurityTab.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { useApi } from '../hooks/useApi'
 import axios from 'axios'
 import JailManager from './JailManager'
